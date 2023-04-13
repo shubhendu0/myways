d789525e6fde16a15fc9b0bf2a08e3ad94b66c37
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-04-13 17:35:30 +0530

    Create myways.js

---
+++ myways.js
@@ -0,0 +1,32 @@
+const output = (num) => {
+    const digitSum = totalSum(num);
+    console.log(digitSum)
+    var result = "";
+    for(let i=1; i<digitSum-1; i++){
+        if(i%4==0 && i%5==0){
+            result += "FizzBuzz ";
+        }
+        else if(i%4==0){
+            result += "Fizz ";
+        }
+        else if(i%5==0){
+            result += "Buzz ";
+        }
+        else{
+            result += i+" ";
+        }
+    }
+    console.log(result);
+}
+
+const totalSum = (num) => {
+    let n = num;
+    let sum = 0;
+    while(n>0){
+        sum += n%10;
+        n = n/10;
+    }
+    return sum;
+}
+
+output(9793331842);

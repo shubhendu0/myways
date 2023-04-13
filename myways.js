6fdf85123355e3561b90ed0907339130b74bc137
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-04-13 17:37:28 +0530

    Update myways.js

--- myways.js
+++ myways.js
@@ -23,8 +23,9 @@ const totalSum = (num) => {
     let n = num;
     let sum = 0;
     while(n>0){
-        sum += n%10;
-        n = n/10;
+        let remainder = n % 10;
+        sum = sum + remainder;
+        n = Math.floor(n/10);
     }
     return sum;
 }
